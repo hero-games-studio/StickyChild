49374c5016803fbc9580135733425ba332abf8f9
ABMecik <35038370+ABMecik@users.noreply.github.com>
2019-07-10 18:14:29 +0300

    mechanic

---
+++ Sticky Child/Assets/Scripts/Lavamovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lavamovement : MonoBehaviour
+{
+
+    public float speed = 15f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(0, speed, 0);
+    }
+}
--- Sticky Child/Assets/Scripts/playerClick.cs
+++ Sticky Child/Assets/Scripts/playerClick.cs
@@ -4,6 +4,24 @@ using UnityEngine;
 
 public class playerClick : MonoBehaviour
 {
+
+    [SerializeField] float diss = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("followPlayer");
+    }
+
+    IEnumerator followPlayer()
+    {
+        while (true)
+        {
+            transform.position = new Vector3(PlayerManager.Instance.player.transform.position.x, PlayerManager.Instance.player.transform.position.y, diss);
+            yield return null;
+        }
+    }
+
+
     private void OnMouseDrag()
     {
         PlayerManager.Instance.player.GetComponent<trajectoryScript>().readyToJump();
